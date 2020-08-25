9fd2070282e921a6923c76153057d4cb097c31ee
Jaaron Nielsen <nielsenjaaron@gmail.com>
2020-08-25 14:12:31 -0600

    added comments

--- CalcLib/Calculator.cs
+++ CalcLib/Calculator.cs
@@ -4,24 +4,52 @@ namespace CalcLib
 {
     public class Calculator
     {
+        /// <summary>
+        /// adds two numbers
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
         public int Add(int firstNumber, int secondNumber)
         {
             return firstNumber + secondNumber;
         }
 
+        /// <summary>
+        /// subtracts two numbers
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
         public int Subtract(int firstNumber, int secondNumber)
         {
             return firstNumber - secondNumber;
         }
 
+        /// <summary>
+        /// multiplies two numbers
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
         public int Mulitply(int firstNumber, int secondNumber)
         {
             return firstNumber * secondNumber;
         }
 
+        /// <summary>
+        /// divides two numbers
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
         public int Divide(int firstNumber, int secondNumber)
         {
-            return firstNumber / secondNumber;
+            if (secondNumber != 0)
+            {
+                return firstNumber / secondNumber;
+            }
+            return 0;
         }
     }
 }
